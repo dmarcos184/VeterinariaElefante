6444414308246d5a1a5ac376732141df9b67dd88
emilio <emilioaguilar054@gmail.com>
2026-05-15 16:22:58 -0600

    productos final

--- gestion/urls.py
+++ gestion/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('servicios/', views.servicios, name='servicios'),
     path('productos/', views.productos, name='productos'),
     path('sucursales/', views.sucursales, name='sucursales'),
+
+    path("ppagina1/", views.ppagina1, name="ppagina1"),
+    path("ppagina2/", views.ppagina2, name="ppagina2"),
+    path("ppagina3/", views.ppagina3, name="ppagina3"),
+    path("ppagina4/", views.ppagina4, name="ppagina4"),
+
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -15,3 +15,14 @@ def productos(request):
 
 def sucursales(request):
     return render(request, 'sucursales.html')
+def ppagina1(request):
+    return render(request, "ppagina1.html")
+
+def ppagina2(request):
+    return render(request, "ppagina2.html")
+
+def ppagina3(request):
+    return render(request, "ppagina3.html")
+
+def ppagina4(request):
+    return render(request, "ppagina4.html")
